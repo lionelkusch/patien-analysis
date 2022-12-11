24e6d4e5e9e22073b3006feb5e6c8b7e697a80a2
Kusch Lionel <lionel.kusch@univ-amu.fr>
2022-12-11 21:30:09 +0100

    update the file for ploting cluster

--- matlab/plot_clusters_allGroup.m
+++ matlab/plot_clusters_allGroup.m
@@ -1,53 +1,63 @@
 close('all');
 clear('all');
-
+addpath('C:\Users\ppsor\Desktop\Code\spm8');
+addpath('C:\Users\ppsor\Desktop\Code\brain_image');
 %% load data
-path = '/home/kusch/Documents/project/patient_analyse/';
-load(strcat(path,'/projection_data/first_projection/all_subject_Y_phate_knn_dist_cosine_mds_dist_cosine_nb_comp_3_nb_pca_5_gamma_-1.0.mat'));
-
-
-%% plot figures
-% parameters 
-groups=cluster_index+1; % index of clusters
+path = 'C:\Users\ppsor\Desktop\studi\Lionel\brains\';
+load(strcat(path,'data_with_cluster.mat'));
 d2 = false; % plot in 2d or 3 dimensions
-plot = false; % save or not the figure
+plot = true; % save or not the figure
 
-for kk1=1:max(groups)
-   patterns(:,kk1)=sum(avalanches_binarize(groups==kk1,:),1);
-   positions{kk1}=PHATE_position(groups==kk1,:);
-end
+%concatenate avalanches
+avalanches_binarize=vertcat(avalanches_binarize{:});
 
-if d2
-    for kk1= 1:size(patterns,2)
-        plot_data(patterns(1:78,kk1), strcat('../paper/result/default/plot_brain/cluster_',int2str(kk1-1),'.png'), plot)
-        fig = figure;
-        scatter(PHATE_position(:,1),PHATE_position(:,2));
-        hold on
-        scatter(positions{kk1}(:,1),positions{kk1}(:,2),'r');
-        hold off
-        fig.Position = [0 0 2000 2000];
-        set(gcf, 'InvertHardCopy', 'off'); 
-        set(gcf,'Color',[0 0 0]); % RGB values [0 0 0] indicates black color
-        if (plot)
-            saveas(gcf, strcat('../paper/result/default/plot_brain/cluster_2D_',int2str(kk1-1),'.png'), plot);
-            close('all');
+for kk_clust=1:size(cluster,1)
+    %% plot figures
+    % parameters
+    groups=cluster{kk_clust,2}+1; % index of clusters
+
+    for kk1=1:max(groups)
+       patterns(:,kk1)=sum(avalanches_binarize(groups==kk1,:),1);
+       positions{kk1}=PHATE_position(groups==kk1,:);
+    end
+
+    if d2
+        % for the 2 dimensionals cluster
+        for kk1= 1:size(patterns,2)
+             % plot brain cluster
+             plot_data(patterns(1:78,kk1), strcat(path, '\k_',int2str(cluster{kk_clust,1}),'cluster_',int2str(kk1-1),'.png'), plot)
+             % plot cluster in low dimension/Phate dimension
+             fig = figure;
+             scatter(PHATE_position(:,1),PHATE_position(:,2));
+             hold on
+             scatter(positions{kk1}(:,1),positions{kk1}(:,2),'r');
+             hold off
+             fig.Position = [0 0 2000 2000];
+             set(gcf, 'InvertHardCopy', 'off');
+             set(gcf,'Color',[0 0 0]); % RGB values [0 0 0] indicates black color
+             if (plot)
+                 saveas(gcf, strcat(path, '\k_',int2str(cluster{kk_clust,1}),'cluster2D_',int2str(kk1-1),'.png'), plot);
+                 close('all');
+             end
         end
-    end    
-else
-     for kk1= 1:size(patterns,2)
-        plot_data(patterns(1:78,kk1), strcat('../paper/result/default/plot_brain/cluster_',int2str(kk1-1),'.png'), plot)
-        fig = figure;
-        scatter3(PHATE_position(:,1),PHATE_position(:,2),PHATE_position(:,3));
-        hold on
-        scatter3(positions{kk1}(:,1),positions{kk1}(:,2),positions{kk1}(:,3),'r');
-        hold off
-        set(gcf, 'InvertHardCopy', 'off'); 
-        set(gcf,'Color',[0 0 0]); % RGB values [0 0 0] indicates black color
-        fig.Position = [0 0 2000 2000];
-        if (plot)
-            saveas(gcf, strcat('../paper/result/default/plot_brain/cluster_2D_',int2str(kk1-1),'.png'))
-            close('all');
-        end
-     end
+    else
+        % for 3 dimensional cluster
+         for kk1= 1:size(patterns,2)
+             % plot brain cluster
+             plot_data(patterns(1:78,kk1),strcat(path, '\k_',int2str(cluster{kk_clust,1}),'cluster_',int2str(kk1-1),'.png'), plot)
+             % plot cluster in low dimension/Phate dimension
+             fig = figure;
+             scatter3(PHATE_position(:,1),PHATE_position(:,2),PHATE_position(:,3));
+             hold on
+             scatter3(positions{kk1}(:,1),positions{kk1}(:,2),positions{kk1}(:,3),'r');
+             hold off
+             set(gcf, 'InvertHardCopy', 'off');
+             set(gcf,'Color',[0 0 0]); % RGB values [0 0 0] indicates black color
+             fig.Position = [0 0 2000 2000];
+             if (plot)
+                 saveas(gcf, strcat(path, '\k_',int2str(cluster{kk_clust,1}),'cluster2D_',int2str(kk1-1),'.png'))
+                 close('all');
+             end
+         end
+    end
 end
-
--- matlab/plot_clusters_significatif.m
+++ matlab/plot_clusters_significatif.m
@@ -1,12 +1,14 @@
 close('all');
 clear('all'); 
 %% library spm8 loading 
-%addpath('C:\Users\ppsor\Desktop\Code\spm8');
-addpath('/home/kusch/Documents/project/patient_analyse/matlab/spm8');
+addpath('C:\Users\ppsor\Desktop\Code\spm8');
+addpath ('C:\Users\ppsor\Desktop\Code\brain_image')
+% addpath('/home/kusch/Documents/project/patient_analyse/matlab/spm8');
 
 %% load data
-path = '/home/kusch/Documents/project/patient_analyse/';
-load(strcat(path,'/paper/result/default/null_model/vector_null_trans.mat'));
+path = 'C:\Users\ppsor\Desktop\studi\Lionel\brains\';
+%% load(strcat(path,'/paper/result/default/null_model/vector_null_trans.mat')); %% data for null model of pipeline
+load(strcat(path,'\cluster_res_vector_null_trans.mat')); %% data for null model of region
 
 nb_cluster = size(transmatrix);
 nb_cluster = nb_cluster(1);
@@ -14,5 +16,5 @@ data = squeeze(transmatrix(:,:,:));
 data(data==0) = NaN;
 
 for kk1=1:nb_cluster
-    plot_data(data(kk1,1:76), strcat(path,'/paper/result/default/plot_brain/cluster_significatif_',int2str(kk1-1),'.png'), false, [1.0, 2.0]);
+    plot_data(data(kk1,1:76), strcat(path,'\cluster_significatif_',int2str(kk1-1),'.png'), true, [1.0, 2.0]);
 end
