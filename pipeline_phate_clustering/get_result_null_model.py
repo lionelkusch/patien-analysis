64563ac3f5bbc12163116ab0fd5c036ab1f900e5
Kusch Lionel <lionel.kusch@univ-amu.fr>
2022-12-12 16:09:20 +0100

    improve figures

--- pipeline_phate_clustering/get_result_null_model.py
+++ pipeline_phate_clustering/get_result_null_model.py
@@ -3,13 +3,29 @@ import numpy as np
 from scipy.stats import entropy
 import scipy.io as io
 
+def get_color_map():
+    """
+    get color map with extreme values in blue or red
+    :return:
+    """
+    # new color bar
+    import matplotlib as mpl
+    from matplotlib.colors import ListedColormap
 
-def plot_pvalue(data, data_num, title='patient : '):
+    color_map_default = mpl.cm.get_cmap('seismic', 128)
+    newcolors = color_map_default(np.linspace(0, 1, 128))
+    newcolors[1:-1] = np.array([1, 1, 1, 1])
+    newcmp = ListedColormap(newcolors, name='extrem')
+    return newcmp
+
+
+def plot_pvalue(data, data_num, title='patient : ', cmap='viridis'):
     """
     plotting array of the matrix with color and number
     :param data: data for the color
     :param data_num: data for the number to show
     :param title: generic title of each subplot
+    :param cmap: colormap for plotting
     :return:
     """
     nb_patient = data.shape[0]
@@ -18,7 +34,8 @@ def plot_pvalue(data, data_num, title='patient : '):
 
     fig, axs = plt.subplots(nb_x, nb_y, figsize=(20, 20))
     for index_patient in range(nb_patient):
-        im = axs[int(index_patient % nb_x), int(index_patient / nb_x)].imshow(data[index_patient], vmin=0.0, vmax=1.0)
+        im = axs[int(index_patient % nb_x), int(index_patient / nb_x)].imshow(data[index_patient],
+                                                                              vmin=0.0, vmax=1.0, cmap=cmap)
         axs[int(index_patient % nb_x), int(index_patient / nb_x)].set_title(title + str(index_patient))
         axs[int(index_patient % nb_x), int(index_patient / nb_x)].autoscale(False)
         fig.colorbar(im, ax=axs[int(index_patient % nb_x), int(index_patient / nb_x)])
@@ -34,14 +51,14 @@ def plot_pvalue(data, data_num, title='patient : '):
 
     for index_no_patient in range(nb_patient, nb_x * nb_y):
         axs[int(index_no_patient % nb_x), int(index_no_patient / nb_x)].imshow(
-            np.ones_like(data[0]) * np.NAN, vmin=0.0, vmax=1.0)
+            np.ones_like(data[0]) * np.NAN, vmin=0.0, vmax=1.0, cmap=cmap)
         axs[int(index_no_patient % nb_x), int(index_no_patient / nb_x)].autoscale(False)
         axs[int(index_no_patient % nb_x), int(index_no_patient / nb_x)].tick_params(
             axis='both', which='both', bottom=False, top=False, labelbottom=False)
     plt.subplots_adjust(left=0.0, right=1.0, wspace=0.0, top=0.975, bottom=0.005, hspace=0.15)
 
 
-def plot_cluster(data, data_num, title='patient : ', vmin=0.0, vmax=1.0):
+def plot_cluster(data, data_num, title='patient : ', vmin=0.0, vmax=1.0, cmap='viridis'):
     """
     plot matrix
     :param data: data for the color
@@ -49,6 +66,7 @@ def plot_cluster(data, data_num, title='patient : ', vmin=0.0, vmax=1.0):
     :param title: title of the figure
     :param vmin: minimum values for the color
     :param vmax: maximum value for the color
+    :param cmap: colormap for plotting
     :return:
     """
     nb_patient = data.shape[0]
@@ -56,7 +74,7 @@ def plot_cluster(data, data_num, title='patient : ', vmin=0.0, vmax=1.0):
 
     fig, axs = plt.subplots(nb_x, 1, figsize=(20, 20))
     if nb_patient == 1:
-        im = axs.imshow(data[0], vmin=vmin, vmax=vmax)
+        im = axs.imshow(data[0], vmin=vmin, vmax=vmax, cmap=cmap)
         axs.set_title(title)
         axs.autoscale(False)
         fig.colorbar(im, ax=axs)
@@ -66,7 +84,7 @@ def plot_cluster(data, data_num, title='patient : ', vmin=0.0, vmax=1.0):
         plt.subplots_adjust(left=0.005, right=1.0, wspace=0.0, top=0.96, bottom=0.005, hspace=0.15)
     else:
         for index_patient in range(nb_patient):
-            im = axs[index_patient].imshow(data[index_patient], vmin=vmin, vmax=vmax)
+            im = axs[index_patient].imshow(data[index_patient], vmin=vmin, vmax=vmax, cmap=cmap)
             axs[index_patient].set_title(title + str(index_patient))
             axs[index_patient].autoscale(False)
             # fig.colorbar(im, ax=axs[index_patient])
@@ -75,7 +93,7 @@ def plot_cluster(data, data_num, title='patient : ', vmin=0.0, vmax=1.0):
             axs[index_patient].tick_params(axis='both', which='both', bottom=False, top=False, labelbottom=False)
         plt.subplots_adjust(left=0.005, right=1.0, wspace=0.0, top=1.0, bottom=0.005, hspace=0.15)
 
-def plot_block_cluster(data, data_num, title='cluster ', vmin=0.0, vmax=1.0, fontsize=2):
+def plot_block_cluster(data, data_num, title='cluster ', vmin=0.0, vmax=1.0, fontsize=2, cmap='viridis'):
     """
     plot matrix
     :param data: data for the color
@@ -83,9 +101,10 @@ def plot_block_cluster(data, data_num, title='cluster ', vmin=0.0, vmax=1.0, fon
     :param title: title of the figure
     :param vmin: minimum values for the color
     :param vmax: maximum value for the color
+    :param cmap: colormap for plotting
     :return:
     """
-    im = plt.imshow(data[0], vmin=vmin, vmax=vmax)
+    im = plt.imshow(data[0], vmin=vmin, vmax=vmax, cmap=cmap)
     plt.title(title)
     # plt.autoscale(False)
     for (j, i), label in np.ndenumerate(data_num[0]):
@@ -129,6 +148,11 @@ def null_model_cluster_regions_res(path_saving, nb_randomize=100, base=None, plo
                  title='p_values')
     if plot:
         plt.savefig(path_saving + '/figure/cluster_res_pvalue.pdf')
+        plt.close('all')
+    plot_block_cluster(pvalue_cluster_all[:, 0, :, :].swapaxes(0, 1), pvalue_cluster_all[:, 0, :, :].swapaxes(0, 1),
+                 title='p_values', cmap=get_color_map())
+    if plot:
+        plt.savefig(path_saving + '/figure/cluster_res_pvalue_rb.pdf')
         plt.close('all')
     plot_block_cluster(pvalue_cluster_all[:, 1, :, :].swapaxes(0, 1), pvalue_cluster_all[:, 0, :, :].swapaxes(0, 1),
                  title='significant')
@@ -221,6 +245,10 @@ def null_model_transition(path_saving, nb_randomize=100, significatif=0.05, plot
     if plot:
         plt.savefig(path_saving + '/figure/transition_patient_pvalue.pdf')
         plt.close('all')
+    plot_pvalue(pvalue, pvalue, cmap=get_color_map())
+    if plot:
+        plt.savefig(path_saving + '/figure/transition_patient_pvalue_rb.pdf')
+        plt.close('all')
     plot_pvalue(significatif_all, pvalue)
     if plot:
         plt.savefig(path_saving + '/figure/transition_patient_pvalue_significatif.pdf')
@@ -262,6 +290,10 @@ def null_model_transition_all(path_saving, nb_randomize=100, significatif=0.05, 
     plot_cluster(pvalue, pvalue, title='transition of all patient')
     if plot:
         plt.savefig(path_saving + '/figure/transition_all_pvalue.pdf')
+        plt.close('all')
+    plot_cluster(pvalue, pvalue, title='transition of all patient', cmap=get_color_map())
+    if plot:
+        plt.savefig(path_saving + '/figure/transition_all_pvalue_rb.pdf')
         plt.close('all')
     plot_cluster(significatif_all, pvalue, title='transition of all patient')
     if plot:
@@ -323,6 +355,11 @@ def null_model_data(path_saving, path_saving_patient, nb_randomize=100, signific
     if plot:
         plt.savefig(path_saving + '/figure/cluster_pvalue.pdf')
         plt.close('all')
+    plot_block_cluster(pvalue_cluster_all[:, 0, :, :].swapaxes(0, 1), pvalue_cluster_all[:, 0, :, :].swapaxes(0, 1),
+                       vmin=significatif, vmax=1-significatif, title='pvalue', cmap=get_color_map())
+    if plot:
+        plt.savefig(path_saving + '/figure/cluster_pvalue_rb.pdf')
+        plt.close('all')
     plot_block_cluster(pvalue_cluster_all[:, 1, :, :].swapaxes(0, 1), pvalue_cluster_all[:, 0, :, :].swapaxes(0, 1),
                        title='significant')
     if plot:
@@ -378,6 +415,80 @@ def null_model_data_entropy(path_saving, path_saving_patient, nb_randomize=100, 
     else:
         plt.show()
 
+def null_model_transition_order(path_saving, nb_randomize=100, significatif=0.05, plot=False):
+    """
+    plot the indicative for comparison transition between patient
+    :param path_saving: path where are the result
+    :param nb_randomize: number of randomisation to take into account
+    :param significatif: acceptation of the percentage to take into account
+    :param plot: save the figure or not
+    :return:
+    """
+    # load value
+    data_null_model = {'transition': [],
+                       'histogram': [],
+                       'cluster_patient': []
+                       }
+    for nb_rand in range(nb_randomize):
+        data_null_model['transition'].append(np.load(path_saving + "/transition" + str(nb_rand) + ".npy"))
+        data_null_model['histogram'].append(np.load(path_saving + "/histograms" + str(nb_rand) + ".npy"))
+        data_null_model['cluster_patient'].append(
+            np.load(path_saving + "/cluster_patient_data" + str(nb_rand) + ".npy", allow_pickle=True))
+
+    data_patient = {'transition': np.load(path_saving + "/transition.npy"),
+                    'histogram': np.load(path_saving + "/histograms_patient.npy"),
+                    'cluster_patient': np.load(path_saving + "/cluster_patient_data.npy", allow_pickle=True)
+                    }
+    nb_cluster = data_patient['transition'].shape[1]
+    nb_patient = data_patient['transition'].shape[0]
+    # compute the pvalue of each transition
+    pvalue = np.sum(np.array(data_null_model['transition']) > data_patient['transition'], axis=0) / nb_randomize
+    significatif_high = pvalue > 1.0 - significatif
+    significatif_low = pvalue < significatif
+    significatif_all = np.logical_or(significatif_low, significatif_high)
+
+    compare_patient = np.sum(significatif_all, axis=0)
+    # compute the variability of indicative transition
+    pvalue = np.array([np.concatenate(data) for data in pvalue])
+    significatif_high = np.array([np.concatenate(data) for data in significatif_high])
+    significatif_low = np.array([np.concatenate(data) for data in significatif_low])
+    significatif_all = np.array([np.concatenate(data) for data in significatif_all])
+    order_patient = np.flip(np.argsort(np.sum(significatif_all, axis=1)))
+    order_transition = np.flip(np.argsort(np.sum(significatif_high, axis=0) - np.sum(significatif_low, axis=0)))
+    name_transition = []
+    for input in range(nb_cluster):
+        for output in range(nb_cluster):
+            name_transition.append('in:'+str(input)+' out:'+str(output))
+
+
+    cmap = get_color_map()
+    fig = plt.figure(figsize=(20, 20))
+    im = plt.imshow(compare_patient/nb_patient, vmax=1.0, vmin=0.0)
+    for (j, i), label in np.ndenumerate(compare_patient/nb_patient):
+        plt.text(i, j, np.around(label, 2), ha='center', va='center', fontsize=10)
+    fig.colorbar(im)
+    if plot:
+        plt.savefig(path_saving + '/figure/transition_percentage_significatif.png')
+        plt.close('all')
+    fig = plt.figure(figsize=(20, 20))
+    im = plt.imshow(pvalue[order_patient, :].T, vmin=significatif, vmax=1-significatif, cmap=cmap)
+    plt.yticks(range(nb_cluster*nb_cluster), name_transition)
+    plt.xticks(range(nb_patient), order_patient)
+    fig.colorbar(im)
+    if plot:
+        plt.savefig(path_saving + '/figure/transition_order_p.png')
+        plt.close('all')
+    fig = plt.figure(figsize=(20, 20))
+    plt.imshow(pvalue[order_patient, :][:, order_transition].T, vmin=significatif, vmax=1-significatif, cmap=cmap)
+    plt.yticks(range(nb_cluster*nb_cluster), np.array(name_transition, dtype=str)[order_transition])
+    plt.xticks(range(nb_patient), order_patient)
+    fig.colorbar(im)
+    if plot:
+        plt.savefig(path_saving + '/figure/transition_order_p_trans.png')
+        plt.close('all')
+    else:
+        plt.show()
+
 
 if __name__ == '__main__':
     null_model_cluster_regions_res(path_saving="/home/kusch/Documents/project/patient_analyse/paper/result/default/",
@@ -385,6 +496,8 @@ if __name__ == '__main__':
                                    # precision/(number of region * number of cluster)
                                    save_mat=True, nb_randomize=10000)
     null_model_transition(path_saving="/home/kusch/Documents/project/patient_analyse/paper/result/default/",
+                          plot=True)
+    null_model_transition_order(path_saving="/home/kusch/Documents/project/patient_analyse/paper/result/default/",
                           plot=True)
     null_model_transition_all(path_saving="/home/kusch/Documents/project/patient_analyse/paper/result/default/",
                               plot=True)
